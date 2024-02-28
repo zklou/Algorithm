f23660e96544dfa34137c070dcf2385f39840c24
zklou <635316569@qq.com>
2024-02-28 01:46:38 -0500

    Update 61lc.py

--- 61lc.py
+++ 61lc.py
@@ -1,16 +1,38 @@
-n-k n>k 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def rotateRight(self, head, k):
+        cur1 = head
+        cur2 = head
+        cur3 = head
+        n = 0
+        i = 0
+        p = 0
+        q = 0
+        while cur1 != None:
+            n = n + 1
+            cur1 = cur1.next
+        if k >= n:
+            k = k % n
 
-if! head return null
+        while i < (n - k):
+            i = i + 1
+            cur2 = cur2.next
+        
+        cur1 = head
+        while p < n - 1:
+            p = p + 1
+            cur1 = cur1.next
+        cur1.next = head
 
-ListNode cur = head
-int n = 1
-while cur.next != null:
-    cur = cur.next
-    n++
-
-    for i=0; i<n-k; i++:
-        cur = cur.next
-    
-    head = cur->next
-cur next = nullptr
-return head 
+        cur1 = head
+        while q < k:
+            q = q + 1
+            cur1 = cur1.next
+        cur3 = cur1.next
+        cur1.next = None
+        
+        return cur3
