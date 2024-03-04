f745bfdf9360452e92b3ee47b782271f386df2e7
zklou <635316569@qq.com>
2024-03-04 01:05:02 -0500

    Update 442lc.py

--- 442lc.py
+++ 442lc.py
@@ -1,7 +1,12 @@
-class Solution(object):
-    def findDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        
+if nums.length=1 then return []
+if nums.length>1 and numcheck=0 return []
+
+
+get first interger as m 
+get second as n
+count as numcheck
+m.length=1 
+ for n.length=m.length+1 n<num.length n++
+ if m=n 
+  m=s
+  numcheck++
